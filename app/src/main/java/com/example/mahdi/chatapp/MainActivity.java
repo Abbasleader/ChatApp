a392b46ca09ccdace990eb0cef10a8bea77af0a3
Mahdi <abbasjaffary33@gmail.com>
2018-10-08 06:51:32 +0430

    Test

---
+++ app/src/main/java/com/example/mahdi/chatapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mahdi.chatapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.text.format.DateFormat;
+
+import com.crashlytics.android.Crashlytics;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import io.fabric.sdk.android.Fabric;
+import io.fabric.sdk.android.services.common.FirebaseInfo;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+    }
+}
